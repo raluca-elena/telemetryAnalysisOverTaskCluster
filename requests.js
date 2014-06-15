3866c5f7285ac4dbb841f12c367dee063281c70c
raluca-elena <ralucaelena1985@gmail.com>
2014-06-14 23:37:01 -0700

    upload/download files s3 bucket

---
+++ requests.js
@@ -0,0 +1,56 @@
+/**
+ * Created by rpodiuc on 6/14/14.
+ */
+var aws = require('aws-sdk');
+aws.config.loadFromPath('./config.json');
+var fs = require('fs');
+var s3 = new aws.S3();
+
+////
+function uploadFile(remoteFilename, fileName) {
+  var fileBuffer = fs.readFileSync(fileName);
+  var metaData = getContentTypeByFile(fileName);
+
+  s3.putObject({
+    ACL: 'public-read',
+    Bucket: 'ralu-telemetry-analysis',
+    Key: remoteFilename,
+    Body: fileBuffer,
+    ContentType: metaData
+  }, function(error, response) {
+    console.log('uploaded file[' + fileName + '] to [' + remoteFilename + '] as [' + metaData + ']');
+    console.log(arguments);
+  });
+}
+
+uploadFile("ralu", "./ralu");
+
+function getContentTypeByFile(fileName) {
+  var rc = 'application/octet-stream';
+  var fn = fileName.toLowerCase();
+
+  if (fn.indexOf('.html') >= 0) rc = 'text/html';
+  else if (fn.indexOf('.txt') >= 0) rc = 'text/txt';
+  else if (fn.indexOf('.json') >= 0) rc = 'application/json';
+  else if (fn.indexOf('.js') >= 0) rc = 'application/x-javascript';
+  else if (fn.indexOf('.png') >= 0) rc = 'image/png';
+  else if (fn.indexOf('.jpg') >= 0) rc = 'image/jpg';
+
+  return rc;
+}
+
+s3.getObject(
+  { Bucket: 'ralu-telemetry-analysis', Key: "ralu" },
+  function (error, data) {
+    if (error != null) {
+      console.log("Failed to retrieve an object: " + error);
+    } else {
+      console.log("Loaded " + data.ContentLength + " bytes");
+      // do something with data.body
+    }
+  }
+);
+
+
+
+
