cb283b959d723f5174683f04e01d0b4bfe44ff2f
raluca-elena <ralucaelena1985@gmail.com>
2014-06-14 22:58:44 -0700

    upload first file s3

--- cl1.js
+++ cl1.js
@@ -1,15 +1,17 @@
 var request = require('superagent');
 var url = 'http://google.com';
 request.get(url, function(response){
-  console.log('Response ok:', response.ok);
+ //console.log('Response ok:', response.ok);
  // console.log('Response text:', response.text);
 });
 var req = request.get(url);
 req.end(function(resp){
-  console.log('Got response', resp.text);
+  //console.log('Got response', resp.text);
 });
+
 request
   .post('http://ec2-54-203-209-235.us-west-2.compute.amazonaws.com:8080/files')
+  .set('Content-Type', 'application/json')
   .send({"filter":{"version":1,"dimensions":[{"field_name":"reason","allowed_values":["saved-session"]},
     {"field_name":"appName","allowed_values":["Fennec"]},
     {"field_name":"appUpdateChannel","allowed_values":["nightly"]},
@@ -17,5 +19,5 @@ request
     {"field_name":"appBuildID","allowed_values":"*"},
     {"field_name":"submission_date","allowed_values":"*"}]}})
   .end(function(resp){
-    console.log('Response ok', resp.text);
+    console.log('Response body: ', resp.body);
   });
