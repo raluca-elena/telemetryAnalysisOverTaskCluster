ef98dfecb38680cc3ceaf2aa7237d1ed8a983198
raluca-elena <ralucaelena1985@gmail.com>
2014-06-16 00:01:19 -0700

    decompress file

---
+++ decompress.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+var lzma = require('lzma').LZMA();
+var r = "saved_session-Fennec-nightly-22.0a1-20130227030925.20131101.v2.log.cc03cd521ba84613808daf1e0d6d3ab6.lzma";
+ fs.readFile(r , function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  lzma.decompress(data, function(d){
+    var data2 =new Buffer(d).toString('utf8');
+    console.log("data2 is", data2);
+    fs.writeFile(r.substr(0, r.length-5), data2);
+  }, function() {console.log("fff");});
+ });
